68d8ea71c23ab2f7acc17ab09f281d849540f95c
Lance Douglas <ldouglas956@gmail.com>
2016-05-17 00:57:35 -0500

    Added potential layout and segue for app views.  Much work to be done with this including potential static table view with dynamic sizing for adding ingredients or steps.

--- Rutherfordium/RecipeCell.swift
+++ Rutherfordium/RecipeCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class RecipeCell: UITableViewCell {
 	
-	@IBOutlet weak var recipeTitle: UITextField!
+	@IBOutlet weak var recipeTitle: UILabel!
 	@IBOutlet weak var recipeImage: UIImageView!
 	
 
